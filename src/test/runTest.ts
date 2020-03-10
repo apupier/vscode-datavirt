64b746250bae901a2b987ae323a080b2aabe2de6
Aurélien Pupier <apupier@redhat.com>
2020-03-10 11:45:18 +0100

    Use correct workspace folder path when launching from command-line #45
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -28,8 +28,14 @@ async function runTest() {
 		// Passed to --extensionTestsPath
 		const extensionTestsPath = path.resolve(__dirname, './');
 
+		const testWorkspace = path.resolve(__dirname, '../../testFixture');
+
 		// Download VS Code, unzip it and run the integration test
-		await runTests({ extensionDevelopmentPath, extensionTestsPath });
+		await runTests({
+			extensionDevelopmentPath,
+			extensionTestsPath,
+			launchArgs: [testWorkspace]
+		});
 	} catch (err) {
 		console.error('Failed to run tests');
 		process.exit(1);
